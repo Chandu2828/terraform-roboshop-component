6c4df74925c916dd41afe0952b65084d1d49d02b
Chandu2828 <purnachand7569@gmail.com>
2026-07-02 22:50:37 +0530

    added files

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ resource "terraform_data" "main" {
 resource "aws_ec2_instance_state" "main"{
     instance_id = aws_instance.main.id 
     state       = "stopped"
-    depends_on  = [terraform_data.catalogue]
+    depends_on  = [terraform_data.main]
 }
 
 # depends_on specified that task3 should execute only after task1
